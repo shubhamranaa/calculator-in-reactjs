64693f16458402ad356e7cffa903377b1bc4aed1
Shubham Rana <shubhamrana9910@gmail.com>
2023-06-27 16:22:21 +0530

    configure React app

---
+++ src/DigitButton.js
@@ -0,0 +1,14 @@
+import { ACTIONS } from "./App"
+import React from "react"
+
+const DigitButton = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
+
+export default DigitButton
